3a18e026e1c6bc1edc074dd3c5c172336911fc22
Yuhan Liu <liu40@ualberta.ca>
2026-01-07 19:38:06 -0700

    Implemented PetShop

---
+++ PetShop/app/src/main/java/com/example/petshop/Mood.java
@@ -0,0 +1,26 @@
+package com.example.petshop;
+
+
+import java.util.Date;
+
+public class Mood {
+    private Date date;
+
+    public Mood(){
+        this(new Date());
+
+    }
+    public Mood(Date date) {
+        this.date = date;
+    }
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public abstract String describe();
+}
+
+
